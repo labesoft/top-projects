22678ae8a5a6675ac6bd23c1e902856e1d0a46d5
benoit.lapointe.3@ens.etsmtl.ca <Benoit Lapointe>
2020-12-09 23:04:18 -0500

    Fixed #10: Completed unit tests

---
+++ hanggame/tests/test_hangman.py
@@ -0,0 +1,173 @@
+"""The tests for hangman.py of The Hangman Game
+-----------------------------------------
+
+About this Project
+------------------
+The objective of this project is to recreate The Hangman Game that a user could
+play interactively by attempting to unmask a word one letter at a time using a
+limited number of attempts without being hanged by the hangman.
+
+About this module
+-----------------
+The objective of this module is to test the hangman module
+"""
+from unittest import TestCase
+from unittest.mock import MagicMock
+
+from hanggame.hangman import *
+from hanggame.level import GameLevel
+
+
+class TestHangman(TestCase):
+    def setUp(self) -> None:
+        self.hangman = Hangman()
+
+    def test___init__(self):
+        """Tests that max_attempt default to beginner value"""
+        # Evaluate test
+        self.assertEqual(self.hangman.max_attempt, GameLevel.BEGINNER.value)
+
+    def test_attempt(self):
+        """Tests that the attempt is max_attempt - missed"""
+        # Run test
+        result = self.hangman.attempt
+
+        # Evaluate test
+        self.assertEqual(result, self.hangman.max_attempt - self.hangman.missed)
+
+    def test_missed(self):
+        """Tests missed getter and properly initialized"""
+        # Run test
+        result = self.hangman.missed
+
+        # Evaluate test
+        self.assertEqual(result, ZERO)
+
+    def test_missed_setter(self):
+        """Tests missed setter"""
+        # Run test
+        self.hangman.missed = 1
+
+        # Evaluate test
+        self.assertEqual(self.hangman.missed, 1)
+
+    def test_draw_init(self):
+        """Tests the first draw is correct and properly initialized"""
+        # Run test
+        self.hangman.draw()
+
+        # Evaluate test
+        self.assertEqual(self.hangman.gallows, GALLOWS)
+        self.assertIsNot(self.hangman.gallows, GALLOWS)
+
+    def test_draw_missed_head(self):
+        """Tests the gallows with head hanged"""
+        # Prepare test
+        self.hangman.missed = 1
+        gallows = list(GALLOWS)
+        gallows[4] = PART_HEAD_HANGED
+
+        # Run test
+        self.hangman.draw()
+
+        # Evaluate test
+        self.assertEqual(self.hangman.gallows, gallows)
+
+    def test_draw_missed_body(self):
+        """Tests the gallows with head/body hanged"""
+        # Prepare test
+        self.hangman.missed = 2
+        gallows = list(GALLOWS)
+        gallows[4] = PART_HEAD_HANGED
+        gallows[5] = PART_BODY
+
+        # Run test
+        self.hangman.draw()
+
+        # Evaluate test
+        self.assertEqual(self.hangman.gallows, gallows)
+
+    def test_draw_missed_left_arm(self):
+        """Tests the gallows with head/left arm hanged"""
+        # Prepare test
+        self.hangman.missed = 3
+        gallows = list(GALLOWS)
+        gallows[4] = PART_HEAD_HANGED
+        gallows[5] = PART_ARM_LEFT
+
+        # Run test
+        self.hangman.draw()
+
+        # Evaluate test
+        self.assertEqual(self.hangman.gallows, gallows)
+
+    def test_draw_missed_arms(self):
+        """Tests the gallows with both arms hanged"""
+        # Prepare test
+        self.hangman.missed = 4
+        gallows = list(GALLOWS)
+        gallows[4] = PART_HEAD_HANGED
+        gallows[5] = PART_ARMS_HANGED
+
+        # Run test
+        self.hangman.draw()
+
+        # Evaluate test
+        self.assertEqual(self.hangman.gallows, gallows)
+
+    def test_draw_missed_left_leg(self):
+        """Tests the gallows with left leg hanged"""
+        # Prepare test
+        self.hangman.missed = 5
+        gallows = list(GALLOWS)
+        gallows[4] = PART_HEAD_HANGED
+        gallows[5] = PART_ARMS_HANGED
+        gallows[6] = PART_LEG_LEFT
+
+        # Run test
+        self.hangman.draw()
+
+        # Evaluate test
+        self.assertEqual(self.hangman.gallows, gallows)
+
+    def test_draw_hanged(self):
+        """Tests the gallows with hanged man"""
+        # Prepare test
+        self.hangman.missed = 6
+        gallows = list(GALLOWS)
+        gallows[4] = PART_HEAD_HANGED
+        gallows[5] = PART_ARMS_HANGED
+        gallows[6] = PART_LEGS_HANGED
+
+        # Run test
+        self.hangman.draw()
+
+        # Evaluate test
+        self.assertEqual(self.hangman.gallows, gallows)
+
+    def test_draw_saved(self):
+        """Tests the gallows with saved man"""
+        # Prepare test
+        gallows = list(GALLOWS)
+        gallows[5] = PART_HEAD_SAVED
+        gallows[6] = PART_BODY
+        gallows[7] = PART_LEGS_SAVED
+
+        # Run test
+        self.hangman.draw(saved=True)
+
+        # Evaluate test
+        self.assertEqual(self.hangman.gallows, gallows)
+
+    def test_reset(self):
+        """Tests the hanged man removal"""
+        # Prepare test
+        self.hangman.missed = 1
+        self.hangman.draw = MagicMock()
+
+        # Run test
+        self.hangman.reset()
+
+        # Evaluate test
+        self.assertEqual(self.hangman.missed, ZERO)
+        self.hangman.draw.assert_called_once()
---
+++ hanggame/tests/test_word.py
@@ -0,0 +1,156 @@
+"""The tests for word.py of The Hangman Game
+-----------------------------------------
+
+About this Project
+------------------
+The objective of this project is to recreate The Hangman Game that a user could
+play interactively by attempting to unmask a word one letter at a time using a
+limited number of attempts without being hanged by the hangman.
+
+About this module
+-----------------
+The objective of this module is to test the word module
+"""
+from unittest import TestCase
+from unittest.mock import patch
+
+from hanggame.word import *
+
+TEST_WORDS = ['A', 'TestWordOne', '123', '_', 'TestWordTwo']
+
+
+class TestWord(TestCase):
+    @patch.object(Word, 'load_words', return_value=TEST_WORDS)
+    def setUp(self, lw) -> None:
+        self.word = Word()
+        self.word.load_words = lw
+
+    def test_load_words(self):
+        """Tests that game has a default 370103 alpha words"""
+        # Prepare test
+        alpha_words_size = 370103
+
+        # Run test
+        words = Word.load_words()
+
+        # Evaluate test
+        self.assertEqual(len(words), alpha_words_size)
+
+    def test_show(self):
+        """Tests that showing the word then empty it"""
+        # Prepare test
+        self.word.choose()
+
+        # Run test
+        self.word.show()
+        w2 = self.word.show()
+
+        # Evaluate test
+        self.assertEqual(w2, EMPTY_WORD)
+
+    def test_unmasked_set(self):
+        """Tests that no letter unmasked at the beginning and that returns a set"""
+        # Prepare test
+        self.word.choose()
+
+        # Run test
+        results = self.word.unmasked_set
+
+        # Evaluate test
+        self.assertEqual(results, set())
+
+    def test_choose(self):
+        """Tests that it chooses a correct word from the list"""
+        # Run test
+        self.word.choose()
+
+        # Evaluate test
+        self.assertIn(self.word.show(), TEST_WORDS)
+
+    def test_is_masked_true(self):
+        """Tests that the word correctly report that it is masked"""
+        # Prepare test
+        self.word.choose()
+
+        # Run test
+        result = self.word.is_masked()
+
+        # Evaluate test
+        self.assertTrue(result)
+
+    def test_is_masked_false(self):
+        """Tests that the word correctly report that it is not masked"""
+        # Prepare test
+        self.word.choose()
+
+        # Run test
+        self.word.show()
+        result = self.word.is_masked()
+
+        # Evaluate test
+        self.assertFalse(result)
+
+    def test_is_unmasked_false(self):
+        """Tests that a masked letter is not reported unmasked"""
+        # Prepare test
+        letter = 'T'
+        self.word.choose()
+
+        # Run test
+        result = self.word.is_unmasked(letter)
+
+        # Evaluate test
+        self.assertFalse(result)
+
+    def test_is_unmasked_true(self):
+        """Tests that an unmasked letter is reported unmasked"""
+        # Prepare test
+        letter = 'T'
+        self.word.choose()
+
+        # Run test
+        self.word.unmask(letter)
+        result = self.word.is_unmasked(letter)
+
+        # Evaluate test
+        self.assertTrue(result)
+
+    def test_unmask_worked(self):
+        """Test if the a letter from the word could be unmasked and reports it properly"""
+        # Prepare test
+        letter = 'T'
+        self.word.choose()
+
+        # Run test
+        worked = self.word.unmask(letter)
+
+        # Evaluate test
+        self.assertIn(letter, str(self.word))
+        self.assertTrue(worked)
+
+    def test_unmasked_already_unmasked(self):
+        """Tests trying to unmask an unmasked letter does not work"""
+        # Prepare test
+        letter = 'T'
+        self.word.choose()
+
+        # Run test
+        self.word.unmask(letter)
+        worked = self.word.unmask(letter)
+
+        # Evaluate test
+        self.assertIn(letter, str(self.word))
+        self.assertFalse(worked)
+
+    def test_unmask_failed_not_in_word(self):
+        """Tests that unmasking a letter not in word fails"""
+        # Prepare test
+        letter = 'Z'
+        self.word.choose()
+
+        # Run test
+        worked = self.word.unmask(letter)
+
+        # Evaluate test
+        self.assertNotIn(letter, str(self.word))
+        self.assertFalse(worked)
