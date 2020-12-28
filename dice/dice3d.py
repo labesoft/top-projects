e81c8b4d4b3545a50d8dfb1044504a312884272d
benoit.lapointe.3@ens.etsmtl.ca <Benoit Lapointe>
2020-12-28 13:52:18 -0500

    Added camera roll task.

--- dice/dice3d.py
+++ dice/dice3d.py
@@ -31,7 +31,10 @@ __date__ = "2020-12-28"
 __copyright__ = "Copyright 2020, labesoft"
 __version__ = "1.0.0"
 
+from math import sin, pi, cos
+
 from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
 
 
 class Dice3D(ShowBase):
@@ -48,6 +51,17 @@ class Dice3D(ShowBase):
         # Apply scale and position transforms on the model.
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
+
+        # Add the spinCameraTask procedure to the task manager.
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
+    # Define a procedure to move the camera.
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 6.0
+        angleRadians = angleDegrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
 
 
 if __name__ == '__main__':
