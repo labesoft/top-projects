bb4e8bf54b57d0e35ba5b1e85a367b7245838173
Benoit Lapointe <benoit.lapointe@labesoft.ca>
2021-05-14 16:57:35 -0400

    Refactored the main module with home page construction.

--- booksys/__main__.py
+++ booksys/__main__.py
@@ -24,68 +24,71 @@ from booksys.issuebook import issue_book
 from booksys.returnbook import return_book
 from booksys.viewbook import view
 
+
+def prepare_image(image_filename, same=True, scale=0.25):
+    image = Image.open(image_filename)
+    width, height = image.size
+    modified_width = int(width * scale)
+
+    if same:
+        modified_height = int(height * scale)
+    else:
+        modified_height = int(height / scale)
+
+    image = image.resize((modified_width, modified_height), Image.ANTIALIAS)
+    return image
+
+
+def create_canvas(canvas_root, image_filename):
+    image = prepare_image(image_filename)
+    background_image = ImageTk.PhotoImage(image)
+    canvas = Canvas(canvas_root)
+    canvas.create_image(300, 340, image=background_image)
+    canvas.config(bg="white", width=image.size[0], height=image.size[1])
+    canvas.pack(expand=True, fill=BOTH)
+
+
+def create_frame(frame_root):
+    frame = Frame(frame_root, bg="#FFBB00", bd=5)
+    frame.place(relx=0.2, rely=0.1, relwidth=0.6, relheight=0.16)
+    heading_label = Label(frame, text="Welcome to \n labesoft Library",
+                          bg='black', fg='white', font=('Courier', 15))
+    heading_label.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+
+def create_root():
+    tk_root = Tk()
+    tk_root.title("labesoft Library")
+    tk_root.minsize(width=400, height=400)
+    tk_root.geometry("600x500")
+    return tk_root
+
+
+def create_button(button_root, btn_text, btn_command, y):
+    btn = Button(
+        button_root, text=btn_text, bg='black', fg='white', command=btn_command
+    )
+    btn.place(relx=0.28, rely=y, relwidth=0.45, relheight=0.1)
+
+
+def create_home_page():
+    root = create_root()
+    create_canvas(root, "lib.jpg")
+    create_frame(root)
+    create_button(root, "Add Book Details", add_book, 0.4)
+    create_button(root, "Delete Book", delete, 0.5)
+    create_button(root, "View Book List", view, 0.6)
+    create_button(root, "Issue Book to Student", issue_book, 0.7)
+    create_button(root, "Return Book", return_book, 0.8)
+    root.mainloop()
+
+
 if __name__ == '__main__':
     """Main entry point of booksys"""
-    # main.py – which does function call to all other python files
-    mypass = "bookuser"  # use your own password
-    mydatabase = "db"  # The database name
-    con = pymysql.connect(host="localhost", user="bookuser", password=mypass,
-                          database=mydatabase)
-    # root is the username here
-    cur = con.cursor()  # cur -> cursor
-
-    root = Tk()
-    root.title("labesoft Library")
-    root.minsize(width=400, height=400)
-    root.geometry("600x500")
-
-    same = True
-    n = 0.25
-    # Adding a background image
-    background_image = Image.open("lib.jpg")
-    [imageSizeWidth, imageSizeHeight] = background_image.size
-    newImageSizeWidth = int(imageSizeWidth * n)
-    if same:
-        newImageSizeHeight = int(imageSizeHeight * n)
-    else:
-        newImageSizeHeight = int(imageSizeHeight / n)
-
-    background_image = background_image.resize(
-        (newImageSizeWidth, newImageSizeHeight), Image.ANTIALIAS)
-    img = ImageTk.PhotoImage(background_image)
-    Canvas1 = Canvas(root)
-    Canvas1.create_image(300, 340, image=img)
-    Canvas1.config(bg="white", width=newImageSizeWidth,
-                   height=newImageSizeHeight)
-    Canvas1.pack(expand=True, fill=BOTH)
-    headingFrame1 = Frame(root, bg="#FFBB00", bd=5)
-    headingFrame1.place(relx=0.2, rely=0.1, relwidth=0.6, relheight=0.16)
-    headingLabel = Label(headingFrame1, text="Welcome to \n labesoft Library",
-                         bg='black', fg='white', font=('Courier', 15))
-    headingLabel.place(relx=0, rely=0, relwidth=1, relheight=1)
-    btn1 = Button(
-        root, text="Add Book Details", bg='black', fg='white', command=add_book
+    mypass = "bookuser"
+    mydatabase = "db"
+    con = pymysql.connect(
+        host="localhost", user="bookuser", password=mypass, database=mydatabase
     )
-    btn1.place(relx=0.28, rely=0.4, relwidth=0.45, relheight=0.1)
-
-    btn2 = Button(
-        root, text="Delete Book", bg='black', fg='white', command=delete
-    )
-    btn2.place(relx=0.28, rely=0.5, relwidth=0.45, relheight=0.1)
-
-    btn3 = Button(
-        root, text="View Book List", bg='black', fg='white', command=view
-    )
-    btn3.place(relx=0.28, rely=0.6, relwidth=0.45, relheight=0.1)
-
-    btn4 = Button(
-        root, text="Issue Book to Student", bg='black', fg='white',
-        command=issue_book
-    )
-    btn4.place(relx=0.28, rely=0.7, relwidth=0.45, relheight=0.1)
-
-    btn5 = Button(
-        root, text="Return Book", bg='black', fg='white', command=return_book
-    )
-    btn5.place(relx=0.28, rely=0.8, relwidth=0.45, relheight=0.1)
-    root.mainloop()
+    cur = con.cursor()
+    create_home_page()
